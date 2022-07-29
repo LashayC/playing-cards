76a60fedfb33c5ec8542598e86556ee86d4a228c
LashayC <cokleylashay@gmail.com>
2022-07-29 17:15:48 -0400

    Changed Rank-first comparator to method reference lambda

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -23,16 +23,18 @@ public class TestMain {
     System.out.println(deck);
 
     //NOTE still this is an example of a strategy.. Rather than creating subclasses of Deck that sort in different way, it used comparators to provide a different strategy of comparator. You can now just use this to keep changing the way the deck is sorted.
-    //NOTE This has been changed from anon class to a lambda function. These curly braces show its a statement lambda. Used anytime there's more than one expression inside lambda
-    //NOTE Compiler knows the two params here are of type Card bc a sort method was written in deck who's param was Comparator<Card>
-    deck.sort((card1, card2) -> {
-      int comparison = card1.getRank().compareTo(card2.getRank());
-      if(comparison == 0){
-        comparison = card1.getSuit().compareTo(card2.getSuit());
-      }
-      return comparison;
-    });
-    System.out.println("Rank first sorter"+ deck);
+    //NOTE replaced lambda with Camparator chain.
+
+    deck.sort(Comparator.comparing(Card::getRank).thenComparing(Card::getSuit));
+    System.out.println("Rank first sorter"+ deck); //NOTE a method reference is any method that takes none or 1 param. That's what the :: do here. (A method reference is still a lambda, jus dif syntax)
+    //NOTE: Because the param on the left side is also being used to invoke a method on the left side, you know the param has all the info needed for the method. You can condense it
+    //NOTE: and tell it to just use the left side to invoke the method on the left. You also exclude the curly braces on the right bc you're just telling the compiler that when its time to invoke the method left side will invoke the method.
+    //NOTE: You can do this w/ mult params as well as long as the order is right and its implementing a Functional object(An object that implements Function(T t) class)
+
+//    deck.sort(Comparator
+//        .comparing((Card card) -> card.getRank()) //NOTE you can expand the method reference out to 2 lambdas by right clicking on Card and expanding it.
+//        .thenComparing(card1 -> card1.getSuit()));
+//    System.out.println("Rank first sorter"+ deck);
 
   }
 
