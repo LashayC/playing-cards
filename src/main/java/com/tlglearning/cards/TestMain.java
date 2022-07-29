e99b488843bfce5cda2ff67903c525132bf592d2
LashayC <cokleylashay@gmail.com>
2022-07-29 16:51:49 -0400

    Rank-first comparator changed to anonymous class in comparator

--- src/main/java/com/tlglearning/cards/TestMain.java
+++ src/main/java/com/tlglearning/cards/TestMain.java
@@ -22,22 +22,21 @@ public class TestMain {
     deck.sort(); //NOTE shows it put back in natural order.
     System.out.println(deck);
 
-    class RankFirstComparator implements Comparator<Card> { //NOTE: Can't put the word static inside a method. Its bc static exists in the context of an entire class but because its not nested in a method, its taking on method context which doesn't jus exist in class.
-        //NOTE this is now a local class because its nested inside a method.
+
+    deck.sort(new Comparator<Card>() {//NOTE you're now passing an instance of Comparator card directly into the .sort method. Now its an anonymous class.
+
       @Override
       public int compare(Card card1, Card card2) {
-        int comparison = card1.getRank().compareTo(card2.getRank());//NOTE we're using the comparator here because the enum implements Comparator and should be able to compare itself.
-        if(comparison == 0){ //TIP if the comparator returns 0, it means their the same and you can then check suits.
+        int comparison = card1.getRank().compareTo(card2.getRank());
+        if(comparison == 0){
           comparison = card1.getSuit().compareTo(card2.getSuit());
         }
         return comparison;
-      }
-      //NOTE this is an example of a strategy.. Rather than creating subclasses of Deck that sort in different way, it used comparators to provide a different strategy of comparator. You can now just use this to keep changing the way the deck is sorted.
-    }
+      }//NOTE this is an example of a strategy.. Rather than creating subclasses of Deck that sort in different way, it used comparators to provide a different strategy of comparator. You can now just use this to keep changing the way the deck is sorted.
 
-
-    deck.sort(new RankFirstComparator());//TIP: Implementing a strategy here with RankFirstComparator
+    });
     System.out.println("Rank first sorter"+ deck);
+
   }
 
 }
